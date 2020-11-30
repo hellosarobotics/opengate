09679b8fdcfe7742fdc19097fa4f6aa283ea840d
Andrea Santopietro <andrea.easycloud@gmail.com>
2020-11-30 12:30:07 +0100

    First import of springboot

---
+++ src/main/java/com/sarobotics/opengate/OpengateApplication.java
@@ -0,0 +1,13 @@
+package com.sarobotics.opengate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpengateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpengateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sarobotics/opengate/OpengateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarobotics.opengate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpengateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
